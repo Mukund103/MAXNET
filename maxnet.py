61beb9fccf322d8c2dfa4a305d30e6f839c10588
Mukund103 <9430mk@gmail.com>
2018-10-21 11:35:29 +0530

    Update maxnet.py

--- maxnet.py
+++ maxnet.py
@@ -1,46 +1,46 @@
-def winner_takes_all(my_list):  
-    array=[]
-    for i in range(0,len(my_list)):
-        if my_list[i]>0:
-            array.append(i)
-    return array
+def winner(mylist):  
+    a=[]
+    for i in range(0,len(mylist)):
+        if mylist[i]>0:
+            a.append(i)
+    return a
     
 m=0     
 delta=0
-y_in=[0] 
+yin=[0] 
 epoch=0    
 f=open("maxnet_input.txt")
-line_number=0
+linenumber=0
 while True:
             line=f.readline()
             line=line.rstrip('\n')
             if len(line)==0:
                 break
-            line_number+=1
+            linenumber+=1
             word=line.split('=')
-            if line_number==1:
+            if linenumber==1:
                 m=int(word[1])
-                y_in=y_in*m
-            if line_number==3:
+                yin=yin*m
+            if linenumber==3:
                 x=word[1].split(',')
                 for i in range(x.__len__()):
-                    y_in[i]=float(x[i])
-            if line_number==2:
+                    yin[i]=float(x[i])
+            if linenumber==2:
                 delta=float(word[1])
                         
 f.close();    
 while True:
     epoch+=1
-    y_out=[]
+    yout=[]
     for i in range(0,m):    
-        if y_in[i]>=0:
-            y_out.append(y_in[i])
+        if yin[i]>=0:
+            yout.append(yin[i])
         else:
-            y_out.append(0)
-    if len(winner_takes_all(y_out))==1:
-        print('winner unit is : ',winner_takes_all(y_out)[0]+1) 
+            yout.append(0)
+    if len(winner(yout))==1:
+        print('winner unit is : ',winner(yout)[0]+1) 
         break
     for i in range(0,m):
-        y_in[i]=y_out[i]-(sum(y_out)-y_out[i])*delta
+        yin[i]=yout[i]-(sum(yout)-yout[i])*delta
     if epoch==100:
         break
